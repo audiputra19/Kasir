04de0a25d0f0a5df17e4ba392a8c4355bb02783b
audiputra19 <audiputra19@gmail.com>
2023-01-05 21:42:27 +0700

    nambahin button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { useHistory } from "react-router";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Admin from "./Admin";
 import Kasir from "./Kasir";
 
 const App = () => {
+  const history = useHistory();
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/"><Kasir /></Route>
-        <Route exact path="/admin"><Admin /></Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <div classsName="flex flex-col gap-5">
+        <button className="p-2 border border-black rounded" onClick={() => history.push("/")}>Kasir</button>
+        <button className="p-2 border border-black rounded" onClick={() => history.push("/admin")}>Admin</button>
+      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"><Kasir /></Route>
+          <Route exact path="/admin"><Admin /></Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   )
 }
 
