4140745a4506d451ff101b27e6118eaf526693d8
dionisius77 <dionisiusadityaoctanugraha@gmail.com>
2023-01-13 19:44:20 +0700

    fix redux config

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,13 +4,10 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
-const middleWares: any = [thunk];
+const middleWares = [thunk];
 if (process.env.NODE_ENV === "development") middleWares.push(logger);
 const finalCreateStore = applyMiddleware(...middleWares)(createStore);
 
 export const store = finalCreateStore(rootReducer, {});
 
 export const persistor = persistStore(store);
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
